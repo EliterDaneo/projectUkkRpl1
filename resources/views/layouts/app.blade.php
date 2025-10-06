7400fac6702360df896b31b2b099679264ea0ea4
eliterdaneo <eliterdaneo13@gmail.com>
2025-10-06 09:56:13 +0700

    perbaikan aplikasi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,15 +75,29 @@
                                             @csrf
                                             <div class="mb-3">
                                                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                                <input type="email" name="email" class="form-control"
+                                                <input type="email" name="email"
+                                                    class="form-control @error('email')
+                                                    is-invalid
+                                                @enderror"
                                                     id="exampleInputEmail1" aria-describedby="emailHelp">
-                                                <div id="emailHelp" class="form-text">We'll never share your email with
-                                                    anyone else.</div>
+                                                @error('email')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
                                             </div>
                                             <div class="mb-3">
                                                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                                                <input type="password" name="password" class="form-control"
+                                                <input type="password" name="password"
+                                                    class="form-control @error('password')
+                                                    is-invalid
+                                                @enderror"
                                                     id="exampleInputPassword1">
+                                                @error('email')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
                                             </div>
                                             <div class="mb-3 form-check">
                                                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
